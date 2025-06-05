680136ad72f630f79eb315564601292fe75a6e93
Kaeliton <cardosokaeliton@gmail.com>
2025-06-05 10:51:09 -0300

    projeto concluido

--- CONTA_NOME/src/ContaTerminal.java
+++ CONTA_NOME/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //variaveis
+        String NomeCliente;
+        int NumeroConta;
+        String AgenciaConta;
+        float SaldoConta;
+
+        Scanner ler = new Scanner(System.in);
+
+        //leitura de dados
+        System.out.println("Por favor, digite o nome do cliente: ");
+        NomeCliente = ler.nextLine();
+
+        System.out.println("digite a agencia do cliente: ");
+        AgenciaConta = ler.nextLine();
+
+        System.out.println("digite o saldo da conta: ");
+        SaldoConta = ler.nextFloat();
+
+        System.out.println("digite o numero da conta: ");
+        NumeroConta = ler.nextInt();
+
+        //mostrando mensagem
+        System.out.println("Olá " + NomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + AgenciaConta + ", conta " + NumeroConta + " e seu saldo " + SaldoConta +  " já está disponível para saque");
     }
 }
